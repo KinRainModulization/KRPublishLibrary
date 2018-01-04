d59c2f4ac40d7f2ae82c361fe21727ac41d0d837
Ace <ohlaixixi@163.com>
2018-01-04 17:23:36 +0800

    新增卡片式轮播

--- KRPublishLibrary.podspec
+++ KRPublishLibrary.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name         = "KRPublishLibrary"
-s.version      = "0.0.8"
+s.version      = "0.0.9"
 s.summary      = "KRPublishLibrary"
 s.description  = <<-DESC
                     KRPublishLibrary.podspec
---
+++ KRPublishLibrary/KRPublishLibrary/Publish/Macros/NotificationMacros.h
@@ -0,0 +1,16 @@
+//
+//  NotificationMacros.h
+//  KRPublishLibrary
+//
+//  Created by LX on 2018/1/4.
+//  Copyright © 2018年 KinRain. All rights reserved.
+//
+
+#ifndef NotificationMacros_h
+#define NotificationMacros_h
+
+#define HOME_SUB_TABLEVIEW_SCROLL @"HomeSubTableViewDidScrollNotification"
+#define HOME_CHANGE_CITY @"HomeChangeCityNotification"
+
+
+#endif /* NotificationMacros_h */
